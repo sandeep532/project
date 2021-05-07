521891d8c98a5b79059bc3b5da78ae31d4d0550d
sandeep532 <sandeepsharmadurbhaka@gmail.com>
2021-05-07 19:44:57 +0530

    Refactor:Changes in result

--- add.sh
+++ add.sh
@@ -2,8 +2,8 @@
 x=100
 y=100
 result=$(($x+$y))
-echo $result
+echo The Result is$result
 read -p "Enter the first number:" num1
 read -p "Enter the second number:" num2
 result2=$(($num1+$num2))
-echo $result2
+echo Result is $result2
